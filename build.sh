60dc2bbe293a9c275858e481bf5d613c6f49ee0b
German Laullon <glaullon@vmware.com>
2020-02-13 20:28:13 +0100

    Nozzle APIv2 (#18)
    * log stream configuration and legacy mode
    
    * new proxy and servicebroker version
    
    * v2.0
    
    Co-authored-by: Vikram Raman <vikram.raman@gmail.com>

--- build.sh
+++ build.sh
@@ -7,14 +7,14 @@ EOM
 
 set -e
 
-PROXY_SOURCE='https://github.com/wavefrontHQ/java/archive/wavefront-4.38.tar.gz'
+PROXY_SOURCE='https://github.com/wavefrontHQ/java/archive/wavefront-5.7.tar.gz'
 PROXY_TGZ='proxy.tgz'
 
-NOZZLE_SOURCE='https://github.com/wavefrontHQ/cloud-foundry-nozzle-go/archive/v1.1.1.tar.gz'
+NOZZLE_SOURCE='https://github.com/wavefrontHQ/cloud-foundry-nozzle-go/archive/v1.2.0.tar.gz'
 # NOZZLE_SOURCE="${HOME}/go/src/github.com/wavefronthq/cloud-foundry-nozzle-go/"
 NOZZLE_TGZ='nozzle.tgz'
 
-BROKER_SOURCE='https://github.com/wavefrontHQ/cloud-foundry-servicebroker/archive/0.9.4.tar.gz'
+BROKER_SOURCE='https://github.com/wavefrontHQ/cloud-foundry-servicebroker/archive/0.9.5.tar.gz'
 # BROKER_SOURCE="${HOME}/wavefront/cloud-foundry-servicebroker/"
 BROKER_TGZ='broker.tgz'
 
